b3975f7c222649bb7399dd88418c08ea9846194c
windard <windard@qq.com>
2021-09-07 10:34:03 +0800

    feat: update subprocess
    Change-Id: Ib095986ad8686baa2c9134797baa7ed07900cdfb

--- code/subprocess_demo.py
+++ code/subprocess_demo.py
@@ -1,11 +1,28 @@
 # coding=utf-8
-
+import os
+import six
 import subprocess
 
+if six.PY2:
+    input = raw_input
+cwd = os.getcwd()
+
+
 def run_command(command):
+    global cwd
     command = command.rstrip()
+    commands = command.split()
+    if commands and commands[0] == "cd":
+        cwd = (len(commands) > 1 and os.path.join(cwd, commands[1])) or os.path.expanduser('~')
+        return b""
+
     try:
-        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+        process = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True, cwd=cwd)
+        stdout, stderr = process.communicate()
+        retcode = process.poll()
+        if retcode:
+            return stderr
+        return stdout
     except:
         output = 'Failed to execute command.\r\n'
 
@@ -13,10 +30,10 @@ def run_command(command):
 
 
 if __name__ == '__main__':
-	while 1:
-		command = raw_input("$ ")
-		if command == "exit" or command == "quit":
-			break
-		result = run_command(command)
-		
-		print result,
+    while 1:
+        command = input("$ ")
+        if command == "exit" or command == "quit":
+            break
+        result = run_command(command)
+        
+        print(result.decode("utf-8"))
